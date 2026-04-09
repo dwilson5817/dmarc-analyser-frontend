703f8299e9542ac585786e070498ea7aaf5d8165
Dylan Wilson <dwilson5817@gmail.com>
2026-04-09 20:55:44 +0000

    feat(components): add DataTable and DataTablePagination components
    These components will be used on the reports pages to show the list of
    reports for a domain, and records for a report.

---
+++ src/components/data-table.tsx
@@ -0,0 +1,62 @@
+import { type Table, flexRender } from "@tanstack/react-table"
+import {
+  Table as UITable,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+interface DataTableProps<TData> {
+  table: Table<TData>
+}
+
+export function DataTable<TData>({ table }: DataTableProps<TData>) {
+  const columns = table.getAllColumns()
+
+  return (
+    <div className="overflow-hidden rounded-md border">
+      <UITable>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <TableHead key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </TableHead>
+              ))}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </UITable>
+    </div>
+  )
+}
