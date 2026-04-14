c7beb2baef4d80f1b1c84daab0ffc70f5f0d39cf
Dylan Wilson <dwilson5817@gmail.com>
2026-04-14 22:03:36 +0000

    feat(hooks): update useGetReports() to accept query parameters
    These will be used by the UI component for pagination and specifying
    reports for a domain between a paticular dates.

--- src/hooks/use-get-reports.ts
+++ src/hooks/use-get-reports.ts
@@ -1,19 +1,30 @@
-import { useQuery } from "@tanstack/react-query"
+import { useQuery, type UseQueryOptions } from "@tanstack/react-query"
 import { useApiClient } from "@/hooks/use-api-client.ts"
+import type { components, paths } from "@/lib/api"
 
-export function useGetReports(domain: string, limit?: number) {
+type QueryParams =
+  paths["/domains/{domain}/reports"]["get"]["parameters"]["query"]
+
+type ReportsResponse = components["schemas"]["ReportsResponse"]
+
+export function useGetReports(
+  domain: string,
+  queryParams: QueryParams,
+  options?: Pick<UseQueryOptions<ReportsResponse>, "enabled" | "staleTime">
+) {
   const client = useApiClient()
   return useQuery({
-    queryKey: ["Reports", domain, limit],
+    queryKey: ["Reports", domain, queryParams],
     queryFn: async () => {
       const { data, error } = await client.GET("/domains/{domain}/reports", {
         params: {
           path: { domain },
-          query: { limit },
+          query: queryParams,
         },
       })
       if (error) throw error
       return data
     },
+    ...options,
   })
 }
