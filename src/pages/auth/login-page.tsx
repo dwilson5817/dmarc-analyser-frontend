a33c19c4b031b77bf31ff2b4d27d42e41819eacc
Dylan Wilson <dwilson5817@gmail.com>
2026-05-03 14:34:34 +0000

    feat(pages): replace GitLab references on login page
    GitLab has been replaced as the authentication provider for this app.
    As a result, replaced the "Login with GitLab" button on the login page
    with a generic "Continue to login" button.  Also updated login page
    title and description as it asked the user to select an authentication
    provider when there is only one.

--- src/pages/auth/login-page.tsx
+++ src/pages/auth/login-page.tsx
@@ -8,6 +8,7 @@ import {
 import { Button } from "@/components/ui/button.tsx"
 import { useAuth } from "react-oidc-context"
 import { Field } from "@/components/ui/field.tsx"
+import { ArrowRight } from "lucide-react"
 
 const LoginPage = () => {
   const { signinRedirect } = useAuth()
@@ -15,8 +16,8 @@ const LoginPage = () => {
   return (
     <Card>
       <CardHeader className="text-center">
-        <CardTitle className="text-xl">Login to continue</CardTitle>
-        <CardDescription>Select an authentication provider</CardDescription>
+        <CardTitle className="text-xl">Authentication required</CardTitle>
+        <CardDescription>Please login to access DMARC Analyzer</CardDescription>
       </CardHeader>
       <CardContent>
         <Field>
@@ -25,17 +26,8 @@ const LoginPage = () => {
             type="button"
             onClick={() => signinRedirect()}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-              data-icon="inline-start"
-            >
-              <path d="m15.734 6.1-.022-.058L13.534.358a.57.57 0 0 0-.563-.356.6.6 0 0 0-.328.122.6.6 0 0 0-.193.294l-1.47 4.499H5.025l-1.47-4.5A.572.572 0 0 0 2.47.358L.289 6.04l-.022.057A4.044 4.044 0 0 0 1.61 10.77l.007.006.02.014 3.318 2.485 1.64 1.242 1 .755a.67.67 0 0 0 .814 0l1-.755 1.64-1.242 3.338-2.5.009-.007a4.05 4.05 0 0 0 1.34-4.668Z" />
-            </svg>
-            Login with GitLab
+            Continue to login
+            <ArrowRight />
           </Button>
         </Field>
       </CardContent>
